6e0413914ca07b2914c31e988a7a2c48eb767fff
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-11-17 17:42:27 -0800

    fixed error for plugin and created first two java classes

---
+++ tech-news-java-api/src/main/java/com/technews/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+//When you create a class, the name of the file must correspond to the name of the class in your code
+public class HelloWorld {
+    //At least one class in every Java program needs a main() method,
+    //which serves as an entry point for the program and gets everything started
+    public static void main(String[] args){ //this method accepts a string array of arguments when the program first starts
+        //println() belongs to the System.out object in Java and is used to print messages to the console
+        System.out.println("Hello, World!");
+    }
+    // IntelliJ IDEA, right-click HelloWorld.java, then select "Run HelloWorld.main()"
+    // similar to JavaScript's 'npm run'
+}
