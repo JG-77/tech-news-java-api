36ffa97c7b15a76eeb2c3adc804568d0284e1bd6
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-11-29 18:06:32 -0800

    set up the post repository interface

--- tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
+++ tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
@@ -1,4 +1,16 @@
 package com.technews.repository;
 
-public interface PostRepository {
+
+import com.technews.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // class-level annotation
+
+// extend the JpaRepository and use <Post, Integer>
+public interface PostRepository extends JpaRepository <Post, Integer>{
+    //create the query method, named findAllPostsByUserId(Integer id), The return type will be List<Post>
+    List<Post> findAllPostsByUserId(Integer id) throws Exception;
 }
